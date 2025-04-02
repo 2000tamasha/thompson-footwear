62779f223d2ee72b37e5ed39130fc302971ca80d
2000tamasha <101210969+2000tamasha@users.noreply.github.com>
2025-04-02 23:00:28 +0800

    Update Login.js
    Added basic html structure for testing

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,6 +4,17 @@ const Login = () => {
   return (
     <div>
       <h2>Login Page</h2>
+    <div className="form-group">
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+          </div>
+    </div>
+   <button type="submit" className="login-button">Login</button>
+       
+        <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+      </div>
     </div>
   );
 };
