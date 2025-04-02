52a41f8e0babe8c7c0186906cd0a2e04d3fc7b5d
thamasha <thamasha.kodithuwakku@gmail.com>
2025-04-03 00:39:33 +0800

    Basic login structure added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login </h2>
-    <div className="form-group">
-            <label htmlFor="email">Email</label>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-          </div>
-    </div>
-   <button type="submit" className="login-button">Login</button>
-       
-        <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+   <div>
+      <h2>Login</h2>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
       </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <button type="submit" className="login-button">Login</button>
+      <p>Don't have an account? <Link to="/register">Sign up</Link></p>
     </div>
   );
 };
