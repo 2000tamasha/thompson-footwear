c6c5524e7aa53e9e0698766023df0ee521d0eeca
thamasha <thamasha.kodithuwakku@gmail.com>
2025-04-07 21:06:49 +0800

    basic sign up content added and login css upadated

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
+
 
 const Register = () => {
-  return (
-    <div>
-      <h2>Register Page</h2>
+<div className="">
+      <h2>Login</h2>
+      <div className="">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+      </div>
+      <div className="">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <button type="submit" className="">Login</button>
+      <p>Already have an account? <Link to="/Login">Sign in</Link></p>
     </div>
   );
-};
+
+
+
+  };
 
 export default Register;
