255a9fcf67744f6d8d395d4793e04b64c58efcee
thamasha <thamasha.kodithuwakku@gmail.com>
2025-04-08 16:07:32 +0800

    register js content added and css imported

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
+import './css/register.css';
 
 
 const Register = () => {
 return(
-<div className="">
+<div className="register-container">
       <h2>Register</h2>
-      <div className="">
+      <div className="form-group">
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" name="email"/>
       </div>
-      <div className="">
+    <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="name" id="name" name="name"/>
+      </div>
+      <div className="form-group">
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
       </div>
-      <button type="submit" className="">Sign Up</button>
-      <p>Already have an account? <Link to="/login">Sign in</Link></p>
+      <button type="submit" className="register-button">Register</button>
+      <p>Already have an account? <Link to="/login">Sign In</Link></p>
     </div>
   );
-
-
 
   };
 
