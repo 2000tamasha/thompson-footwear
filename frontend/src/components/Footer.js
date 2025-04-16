2d803c9d4c392a154116f2d5cabac4741a6e3aba
thamasha <thamasha.kodithuwakku@gmail.com>
2025-04-17 00:53:23 +0800

    footer css created

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+// Footer.js – Created by Thamasha Kodithuwakku 24351177
+
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+ 
+
+  return (
+    <footer className="footer">
+    <p>© {new Date().getFullYear()} Thompson Footwear. All rights reserved.</p>
+  </footer>
+  );
+};
+
+export default Footer;
